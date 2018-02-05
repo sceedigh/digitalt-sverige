0cd7560bfd5cac2aaaa1bcc83d817c2cca4d97e7
Sven-Erik Ceedigh <sceedigh@gmail.com>
2018-02-05 21:39:16 +0100

    Visa MM-anslutna företag i procent

--- src/assets/js/stat-foretag.js
+++ src/assets/js/stat-foretag.js
@@ -1,7 +1,8 @@
 // Företagsstatistik från Bolagsverket (API saknas):
 // http://www.bolagsverket.se/be/sok/etjanster/statistik/statistik-1.3538
 // Aktuella bolagsformer för Mina meddelanden: AB, KB, HB, BRF, EK
-  var foretag_data = {
+var foretag_antal;
+var foretag_data = {
 	"foretag": [
 	  {"datum": "2016-01", "antal": 631691},
 	  {"datum": "2017-01", "antal": 656423},
@@ -11,6 +12,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
-	var foretag_antal = foretag_data.foretag[2].antal.toLocaleString();
-  document.getElementById("stat-foretag").textContent = foretag_antal + " företag är registrerade i Sverige.";
+	foretag_antal = foretag_data.foretag[2].antal;
+  document.getElementById("stat-foretag").textContent = foretag_antal.toLocaleString() + " företag är registrerade i Sverige.";
 });
--- src/assets/js/stat-mm.js
+++ src/assets/js/stat-mm.js
@@ -1,10 +1,11 @@
 // Mina meddelanden - antal innehavare av digitala brevlådor för myndighetspost (Öppet API från Skatteverket)
 var mm_data_mottagare_url = "https://skatteverket.entryscape.net/rowstore/dataset/2927e831-9075-4bc0-b3a2-3336a992ca4b/json";
 var mm_data_avsandare_url = "https://skatteverket.entryscape.net/rowstore/dataset/120b9d9d-e509-4801-836f-c4444cf81fcc/json";
-var mm_antal_privat;
-var mm_antal_foretag;
-var mm_myndigheter;
-var mm_kommuner;
+var mm_privat_antal;
+var mm_foretag_antal;
+var mm_myndigheter_antal;
+var mm_kommuner_antal;
+var mm_foretag_procent;
 var mm_myndigheter_procent;
 var mm_kommuner_procent;
 
@@ -30,11 +31,12 @@ function fetchMMMottagare() {
 
 					var mm_aktuell = data.results["0"];
 
-					mm_antal_privat = (parseInt(mm_aktuell.privatpersoner.replace(/\s/g, "")) / 1000000).toFixed(1);
-					mm_antal_foretag = parseInt(mm_aktuell.företag.replace(/\s/g, "")).toLocaleString();
+					mm_privat_antal = (parseInt(mm_aktuell.privatpersoner.replace(/\s/g, "")) / 1000000).toFixed(1);
+					mm_foretag_antal = parseInt(mm_aktuell.företag.replace(/\s/g, ""));
+					mm_foretag_procent = Math.round((mm_foretag_antal/foretag_antal)*100);
 
-					document.getElementById("stat-mm-privat").textContent = mm_antal_privat + " miljoner privatpersoner har en säker digital brevlåda för myndighetspost.";
-					document.getElementById("stat-mm-foretag").textContent = mm_antal_foretag + " företag har en säker digital brevlåda för myndighetspost.";
+					document.getElementById("stat-mm-privat").textContent = mm_privat_antal + " miljoner privatpersoner har en säker digital brevlåda för myndighetspost.";
+					document.getElementById("stat-mm-foretag").textContent = mm_foretag_antal.toLocaleString() + " företag (" + mm_foretag_procent + " %) har en säker digital brevlåda för myndighetspost.";
 			  });
 			}
 	  )
@@ -60,11 +62,11 @@ function fetchMMAvsandare() {
 
 					var mm_aktuell = data.results["0"];
 
-					mm_myndigheter = mm_aktuell.myndigheter;
-					mm_kommuner = mm_aktuell.kommuner;
+					mm_antal_myndigheter = mm_aktuell.myndigheter;
+					mm_antal_kommuner = mm_aktuell.kommuner;
 
-					mm_myndigheter_procent = Math.round((mm_myndigheter/myndigheter_antal)*100);
-					mm_kommuner_procent = Math.round((mm_kommuner/kommuner_antal)*100);
+					mm_myndigheter_procent = Math.round((mm_antal_myndigheter/myndigheter_antal)*100);
+					mm_kommuner_procent = Math.round((mm_antal_kommuner/kommuner_antal)*100);
 
 					document.getElementById("stat-mm-myndigheter").textContent = mm_myndigheter_procent + " %";
 					document.getElementById("stat-mm-kommuner").textContent = mm_kommuner_procent + " %";
