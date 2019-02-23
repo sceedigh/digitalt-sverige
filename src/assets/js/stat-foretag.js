e038b7bdd575899cb6338b6c357f8f7c8332b4f1
Sven-Erik Ceedigh <sceedigh@gmail.com>
2019-02-23 13:58:23 +0100

    Ny statistik från Bolagsverket 2019-01

--- src/assets/js/stat-foretag.js
+++ src/assets/js/stat-foretag.js
@@ -7,7 +7,7 @@ var foretag_data = {
 	  {"datum": "2016-01", "antal": 631691},
 	  {"datum": "2017-01", "antal": 656423},
 	  {"datum": "2018-01", "antal": 682279},
-		{"datum": "2018-08", "antal": 692239},
+		{"datum": "2019-01", "antal": 703667},
 	]
 };
 
