681c0777ddc100d8c80f4ed4ad97bcf24beb2701
Sven-Erik Ceedigh <sceedigh@gmail.com>
2020-05-09 15:23:16 +0200

    Uppdaterat företagsstatistik

--- src/assets/js/stat-foretag.js
+++ src/assets/js/stat-foretag.js
@@ -7,7 +7,7 @@ var foretag_data = {
 	  {"datum": "2016-01", "antal": 631691},
 	  {"datum": "2017-01", "antal": 656423},
 	  {"datum": "2018-01", "antal": 682279},
-		{"datum": "2019-01", "antal": 703667},
+		{"datum": "2020-04", "antal": 733206},
 	]
 };
 
@@ -15,4 +15,5 @@ document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
 	foretag_antal = foretag_data.foretag[3].antal;
   document.getElementById("stat-foretag").textContent = foretag_antal.toLocaleString() + " företag är registrerade i Sverige.";
+	document.getElementById("stat-foretag-datum").textContent = "(" + foretag_data.foretag[3].datum + ")";
 });
