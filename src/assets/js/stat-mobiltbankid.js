fc1b91d9ec31deaa85071b6230410963dcab233d
Sven-Erik Ceedigh <sceedigh@gmail.com>
2020-05-09 15:25:33 +0200

    Uppdaterat BankID-statistik och lagt till uppdateringsdatum

--- src/assets/js/stat-mobiltbankid.js
+++ src/assets/js/stat-mobiltbankid.js
@@ -5,11 +5,15 @@
 		{"datum": "2016-01", "antal": 4800000},
 		{"datum": "2017-01", "antal": 5900000},
 		{"datum": "2018-09", "antal": 7000000},
+		{"datum": "2020-04", "antal": 7500000},
 	]
 };
 
+//var mobiltbankid_datum = mobilt_bankid_data.innehavare[3].datum;
+
 document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
-	var mobiltbankid_antal = (mobilt_bankid_data.innehavare[2].antal / 1000000).toFixed(1);
+	var mobiltbankid_antal = (mobilt_bankid_data.innehavare[3].antal / 1000000).toFixed(1);
   document.getElementById("stat-mobiltbankid").textContent = mobiltbankid_antal + " miljoner privatpersoner har ett Mobilt BankID.";
+	document.getElementById("stat-mobiltbankid-datum").textContent = "(" + mobilt_bankid_data.innehavare[3].datum + ")";
 });
